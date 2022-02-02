f9f87977cef486308b0e5813e8004d9a74a4e617
Pedro Carara <cararax@gmail.com>
2022-02-02 08:26:52 -0300

    Projeto iniciado com Spring Initializer

---
+++ src/test/java/com/carara/forumapi/ForumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carara.forumapi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
