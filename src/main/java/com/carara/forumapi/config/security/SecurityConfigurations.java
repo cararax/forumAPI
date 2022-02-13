7f19332dfa7142fcba1b9f883d4b80bd206a358b
Pedro Carara <cararax@gmail.com>
2022-02-13 09:43:39 -0300

    Adicionado documentação com Swagger

--- src/main/java/com/carara/forumapi/config/security/SecurityConfigurations.java
+++ src/main/java/com/carara/forumapi/config/security/SecurityConfigurations.java
@@ -51,6 +51,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Override //configurações de recursos estaticos
     public void configure(WebSecurity web) throws Exception {
-        super.configure(web);
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 }
